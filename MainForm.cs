a126d5d79d85130c9170adb7f5fafe41afb0dad9
Konstantin <bodrov.2005@inbox.ru>
2024-11-20 16:10:55 +0500

    Доработал адаптив в MainForm(Съезжают даты) Добавтл список недель

--- MainForm.cs
+++ MainForm.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace Student_Note
 {
@@ -17,16 +19,47 @@ namespace Student_Note
             InitializeComponent();
         }
 
-        private /*async*/ void MainForm_Load(object sender, EventArgs e)
+        private async void MainForm_Load(object sender, EventArgs e)
         {
             // Вызов функции, которая загрузит и выведет данные
-            //await new Server().GetScheduleAsync();
-            //Data_Base.DBConnection();
-        }
+            await new Server().GetScheduleAsync();
+            Data_Base.DBConnection();
 
-        private void HeaderLabel_Click(object sender, EventArgs e)
-        {
+            // Получение списка недель 
+            DateTime startDate = new DateTime(DateTime.Now.Year, 9, 2);
+            DateTime endDate = new DateTime(DateTime.Now.Year + 1, 5, 31);
+            List<Week> weeks = new List<Week>();
+            int id = 0;
+            for (DateTime date = startDate; date <= endDate; date = date.AddDays(7))
+            {
+                Week week = new Week
+                {
+                    Id = id,
+                    WeekNumber = id+1,
+                    StartDate = date,
+                    EndDate = date.AddDays(6)
+                };
+                weeks.Add(week);
+                id++;
+            }
 
+            foreach (var week in weeks)
+            {
+                string weekString = $"{week.WeekNumber} неделя ({week.StartDate.ToShortDateString()} - {week.EndDate.ToShortDateString()})";
+                WeekComboBox.Items.Add(weekString);
+            }
+
+            DateTime currentDate = DateTime.Now;
+            int currentWeekId = 0;
+            foreach (var week in weeks)
+            {
+                if (currentDate >= week.StartDate && currentDate <= week.EndDate)
+                {
+                    currentWeekId = week.Id;
+                    break;
+                }
+            }
+            WeekComboBox.SelectedIndex = currentWeekId;
         }
     }
 }
